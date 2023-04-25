6a212627665c579e01c2295432ada52586b24019
Bdoor-K3 <79381393+Bdoor-K3@users.noreply.github.com>
2023-04-25 06:00:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let weather = {
+    apiKey: "235ca0f68d36e0f6d87848ff817dbb26",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("Sorry No weather found.");
+            throw new Error("Sorry No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity, temp_min, temp_max } = data.main;
+      const min = temp_min;
+      const max = temp_max;     
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "" + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      
+  document.querySelector(".min").innerText = min + "°";
+  document.querySelector(".max").innerText = max + "°";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  weather.fetchWeather("Dammam");
